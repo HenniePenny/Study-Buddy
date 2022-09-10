4dbb5e68d1bb43887f1160bbc1c238a811f27f99
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-09-10 17:03:06 +0200

    Add AdminJS packagage requirements for server.js

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+const AdminJS = require('adminjs');
+const AdminJSExpress = require('@adminjs/express');
+const AdminJSSequelize = require('@adminjs/sequelize');
+AdminJS.registerAdapter(AdminJSSequelize);
+
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
@@ -11,17 +16,21 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Create express router for AdminJS package routes
+const adminJs = new AdminJS({ databases: [], rootPath: '/admin' });
+const router = AdminJSExpress.buildRouter(adminJs);
+
 //! Do we need to set up Handlebars.js engine with custom helpers?
 const hbs = exphbs.create({ helpers });
 
-//! What does sess do? 
+//! What does sess do?
 const sess = {
   secret: 'Super secret secret',
   cookie: {
     maxAge: 300000,
     httpOnly: true,
     secure: false,
-    sameSite: 'strict',
+    sameSite: 'strict'
   },
   resave: false,
   saveUninitialized: true,
@@ -35,6 +44,10 @@ app.use(session(sess));
 // Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+// Use adminJS package routing
+app.use(adminJs.options.rootPath, router);
+app.listen(8080, () => console.log('AdminJS is under localhost:8080/admin'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
