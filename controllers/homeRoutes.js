e4e76bfac7f56f969e4f526dca7ae50cf32d2853
HenniePenny <h.kressner@hotmail.de>
2022-09-12 22:04:07 +0200

    initial test for handlebars templates and adminlte

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,6 +2,11 @@ const router = require('express').Router();
 // const { User } = require('../models');
 // const withAuth = require('../utils/auth');
 
+//this route will serve the user dashboard based on type of logged in user
+router.get('/', async (req, res) => {
+  res.render('dashboard');
+});
+
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
