101069a7f9bd4512e6b7b3f0fcc2a9e8b958639f
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-09-14 16:45:03 +0200

    Move adminLTE assets

--- server.js
+++ server.js
@@ -1,8 +1,3 @@
-const AdminJS = require('adminjs');
-const AdminJSExpress = require('@adminjs/express');
-const AdminJSSequelize = require('@adminjs/sequelize');
-AdminJS.registerAdapter(AdminJSSequelize);
-
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
@@ -15,10 +10,6 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-// Create express router for AdminJS package routes
-const adminJs = new AdminJS({ databases: [], rootPath: '/admin' });
-const router = AdminJSExpress.buildRouter(adminJs);
 
 //! Do we need to set up Handlebars.js engine with custom helpers?
 const hbs = exphbs.create({ helpers });
@@ -44,10 +35,6 @@ app.use(session(sess));
 // Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-
-// Use adminJS package routing
-app.use(adminJs.options.rootPath, router);
-app.listen(8080, () => console.log('AdminJS is under localhost:8080/admin'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
