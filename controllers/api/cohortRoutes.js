d6cc0df0546ce7e756f4db67f3f48e19cdb14e86
HenniePenny <h.kressner@hotmail.de>
2022-09-15 19:15:07 +0200

    formatting changes in routes (eslint)

--- controllers/api/cohortRoutes.js
+++ controllers/api/cohortRoutes.js
@@ -9,27 +9,27 @@ const withAuth = require('../../utils/auth');
 
 // Get All Cohorts
 router.get('/', withAuth, async (req, res) => {
- res.json('this route gives all cohort')
+  res.json('this route gives all cohort');
 });
 
 // Add a new cohort
 router.post('/', withAuth, async (req, res) => {
-    res.json('add new cohort')
-})
+  res.json('add new cohort');
+});
 
 // Get details of a single cohort
 router.get('/:cohortId', withAuth, async (req, res) => {
-    res.json('details of a single  cohort')
-})
+  res.json('details of a single  cohort');
+});
 
 // Add Multiple students to cohort
 router.post('/:cohortId/add-students', withAuth, async (req, res) => {
-    res.json('Add Multiple students to cohort')
-} )
+  res.json('Add Multiple students to cohort');
+});
 
 // Delete a new cohort
 router.delete('/:cohortId', withAuth, async (req, res) => {
-    res.json('Delete a new cohort')
-})
+  res.json('Delete a new cohort');
+});
 
 module.exports = router;
