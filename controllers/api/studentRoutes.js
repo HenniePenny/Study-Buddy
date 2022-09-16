864ba1a4a04f8d8b2fd7f85f53a54d19125a59c9
erog-tech <prikshitthakur0@gmail.com>
2022-09-16 19:16:56 +0200

    Create User and homeroutes

--- controllers/api/studentRoutes.js
+++ controllers/api/studentRoutes.js
@@ -7,12 +7,12 @@ const withAuth = require('../../utils/auth');
 
 // Add a student to a given Cohort
 router.post('/', withAuth, async (req, res) => {
-    res.json('Add a student to a given Cohort')
+  res.json('Add a student to a given Cohort');
 });
 
 // Delete a student
 router.delete('/:studentId', withAuth, async (req, res) => {
-    res.json('Delete a student')
-})
+  res.json('Delete a student');
+});
 
 module.exports = router;
--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -2,6 +2,27 @@ const router = require('express').Router();
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
+router.get('/program-managers', async (req, res) => {
+  try {
+    const userData = await User.findAll({
+      attributes: { exclude: ['password'] },
+      order: [['firstName', 'ASC']],
+    });
+
+    const users = userData.map((project) => project.get({ plain: true }));
+
+    console.log(users)
+    res.json(users)
+    // res.render('program-manager-layout', {
+    //   users,
+
+    //   logged_in: req.session.logged_in,
+    // });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
