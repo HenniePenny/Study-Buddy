5813d7585cac35c11196d4e3ac6cf5f7cdba3b54
tornicke <torniqe@gmail.com>
2022-09-18 21:09:46 +0200

    Adding comments and fixing comments

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,9 +3,9 @@ const studentData = require('../seeds/studentData.json');
 const generateGroups = (students, groupSize) => {
   const numberOfGroups = Math.floor(students.length / groupSize);
   // console.log('numberOfGroups', numberOfGroups);
-
   // const shuffledArray = students.sort(() => Math.random() - 0.5);
-  // check that no group member of the same nationality is added to the same group in a row
+
+  // check that students who share the same nationality are assigned to different groups, unless the number of students with the same nationality exceeds the number of groups
   const shuffledArray = students.sort((a, b) =>
     a.nationality > b.nationality ? 1 : -1
   );
@@ -14,6 +14,7 @@ const generateGroups = (students, groupSize) => {
   //   return a.nationality > b.nationality ? 1 : -1;
   // });
 
+  // assign different group numbers to students, the group number should vary from 1 to the number of groups
   const distributedGroups = shuffledArray.map((student, index) => {
     // console.log('index', index);
     // console.log('index % numberOfGroups', index % numberOfGroups);
@@ -23,6 +24,7 @@ const generateGroups = (students, groupSize) => {
     };
   });
 
+  // gender_counter shows the distribution of gender identities across groups
   const gender_counter = {};
   // populate group numbers with their gender count
   distributedGroups.forEach((el) => {
@@ -35,10 +37,11 @@ const generateGroups = (students, groupSize) => {
     } else {
       gender_counter[el.group_number] = { [el.gender]: 1 };
     }
+    console.log(gender_counter);
   });
 
-  // check if there are groups that only contain members of one gender
   const halfGroupSize = groupSize / 2;
+  // check if there are groups that only contain members of one gender
   Object.keys(gender_counter).forEach((group) => {
     if (gender_counter[group]['MALE'] === groupSize) {
       console.log('All male', group);
